29de5140a59b442954415f0d4d410fb7e387ccd3
Yeiner <ys.lopez28@ciaf.edu.co>
2025-10-01 19:37:17 -0500

    .env

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
 app.get('/', (req, res) => {
     res.json({
         message: '¡Hola! Express funcionando',
